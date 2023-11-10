a35de15d394f0b7e21b492e695295e462c7a0985
Liliane de Freitas Terra Vieira <144692845+lilianefreitas-tv@users.noreply.github.com>
2023-11-10 11:11:54 -0300

    Update inventariobd.sql

--- InvetarioTI/inventariobd.sql
+++ InvetarioTI/inventariobd.sql
@@ -90,6 +90,7 @@ CREATE TABLE `material` (
   `ip` varchar(20) NOT NULL,
   `obs` varchar(150) NOT NULL,
   `cidade` varchar(50) NOT NULL
+   `status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
